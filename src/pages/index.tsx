1755e89c3770c9d739eaab1fa70fb462431db171
Phil Fevry <pf@phlfvry.com>
2020-08-12 01:15:20 -0700

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import tw from 'twin.macro'
+import styled from 'styled-components'
+
+const Container = tw.div`container mx-auto`
+const List = styled.ul`
+  list-style: disc;
+`
+
+const IndexPage = () => (
+  <Container>
+    <h1>AURORA Digital</h1>
+    <h2>Gatsby Starter</h2>
+    <div>
+      Features:
+      <List>
+        <li>Eslint</li>
+        <li>Prettier</li>
+        <li>Stylelint</li>
+        <li>Tailwind CSS</li>
+        <li>PostCSS</li>
+        <li>TypeScript</li>
+        <li>Styled Components</li>
+      </List>
+    </div>
+  </Container>
+)
+
+export default IndexPage
