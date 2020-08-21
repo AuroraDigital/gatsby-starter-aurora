965cbf6ff425e4991acc78996a14265ae00829e9
Phil Fevry <pf@phlfvry.com>
2020-08-21 16:05:23 -0700

    add gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-postcss',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
